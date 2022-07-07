845fc435c74eca470d90bc6022e298843fd348b3
kokurisann <82989642+kokurisann@users.noreply.github.com>
2022-07-07 21:32:19 +0800

    完成PermissionX库的开发与测试工作。

---
+++ library/src/main/java/com/permissionx/library_kokurisann/InvisibleFragment.kt
@@ -0,0 +1,40 @@
+@file:Suppress("DEPRECATION")
+
+package com.permissionx.library_kokurisann
+
+import android.content.pm.PackageManager
+import androidx.fragment.app.Fragment
+
+//使用typealias给任意类型制定一个别名
+typealias PermissionCallback = (Boolean, List<String>) -> Unit
+
+class InvisibleFragment : Fragment() {
+
+    private var callback: PermissionCallback? = null
+
+
+    fun requestNow(cb: PermissionCallback, vararg permissions: String) {
+        callback = cb
+        requestPermissions(permissions, 1)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == 1) {
+            val deniedList = ArrayList<String>()
+            for ((index, result) in grantResults.withIndex()) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    deniedList.add(permissions[index])
+                }
+            }
+            val allGranted = deniedList.isEmpty()
+            callback?.let { it(allGranted, deniedList) }
+        }
+    }
+
+}
+
